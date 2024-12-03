e324b2a2be600345433cd555ebe5afc5db7bbdf2
ANAS <anaspappadan@gmail.com>
2024-12-03 16:36:39 +0530

    feat: add MySQL integration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,26 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import pool from "./configs/db";
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM users");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something broke!" });
 });
 
 app.listen(port, () => {
